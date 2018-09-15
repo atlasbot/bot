15f004c137985e9f5323ec01aeb38b87eeccf75a
Nightlife <captainminenerd@gmail.com>
2018-09-16 07:03:52 +0800

    kinda irrelevant now

--- src/commands/Moderation/move.js
+++ src/commands/Moderation/move.js
@@ -11,7 +11,6 @@ module.exports = class Move extends Command {
 		settings, // eslint-disable-line no-unused-vars
 		parsedArgs,
 	}) {
-		// TODO: add ban to guild event thingy in guild struct and handle logging from here instead of action log
 		const responder = new this.Atlas.structs.Responder(msg);
 
 		if (!args[0]) {
