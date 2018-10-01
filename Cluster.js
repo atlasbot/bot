4feba3b04bc50f1ce800ca3c08cce5800cbe5fd0
Nightlife <captainminenerd@gmail.com>
2018-10-02 05:31:15 +0800

    client options that i deleted earlier for some reason

--- Cluster.js
+++ Cluster.js
@@ -6,12 +6,19 @@ require('dotenv').config();
 new Manager(process.env.TOKEN, path.join(__dirname, './Atlas.js'), { // eslint-disable-line no-new
 	guildsPerShard: 2000,
 	clientOptions: {
+		// atlas uses rest mode for fetching users from ID's, among other things
 		restMode: true,
+		// permformance reasons
+		// i am leaving that typo if you're wondering
 		disableEvents: {
 			TYPING_START: true,
 			USER_NOTE_UPDATE: true,
 			RELATIONSHIP_ADD: true,
 			RELATIONSHIP_REMOVE: true,
 		},
+		// transparent avatars look gross with the default jpg format
+		defaultImageFormat: 'png',
+		// for embed thumbnails it looks kinda gross <256
+		defaultImageSize: 256,
 	},
 });
