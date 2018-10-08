afa5ce6f5d5dc5406df1985bb16489739348f55e
Nightlife <captainminenerd@gmail.com>
2018-10-09 04:02:39 +0800

    commands(utilities): "joined" command

---
+++ src/commands/Utilities/joined.js
@@ -0,0 +1,41 @@
+const Command = require('../../structures/Command.js');
+
+module.exports = class Joined extends Command {
+	constructor(Atlas) {
+		super(Atlas, module.exports.info);
+	}
+
+	async action(msg, args, { // eslint-disable-line no-unused-vars
+		settings, // eslint-disable-line no-unused-vars
+	}) {
+		const responder = new this.Atlas.structs.Responder(msg);
+
+		if (!args[0]) {
+			return responder.error('joined.noArgs').send();
+		}
+
+		const user = await settings.findMember(args.join(' '), {
+			memberOnly: true,
+		});
+
+		if (!user) {
+			return responder.error('joined.noUser', args.join(' ')).send();
+		}
+
+		return responder.text(
+			'joined.message',
+			user.mention,
+			msg.guild.name,
+			new Date(user.joinedAt).toLocaleString(),
+		).send();
+	}
+};
+
+module.exports.info = {
+	name: 'joined',
+	examples: [
+		'@user',
+		'111372124383428608',
+	],
+	guildOnly: true,
+};
