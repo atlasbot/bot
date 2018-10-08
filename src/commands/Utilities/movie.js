53b462d7cbc2fcb355e8bd65924d772f52487fe2
Nightlife <captainminenerd@gmail.com>
2018-10-09 07:44:16 +0800

    commands(utilities): "move" now uses async/await
    it looks cleaner ¯\_(ツ)_/¯

--- src/commands/Utilities/movie.js
+++ src/commands/Utilities/movie.js
@@ -6,7 +6,7 @@ module.exports = class Movie extends Command {
 		super(Atlas, module.exports.info);
 	}
 
-	action(msg, args, {
+	async action(msg, args, {
 		settings, // eslint-disable-line no-unused-vars
 	}) {
 		const responder = new this.Atlas.structs.Responder(msg);
@@ -15,71 +15,67 @@ module.exports = class Movie extends Command {
 			return responder.embed(this.helpEmbed(msg)).send();
 		}
 
-		superagent.get('https://private.omdbapi.com')
+		const { body } = await superagent.get('https://private.omdbapi.com')
 			.query({
 				t: args.join(' '),
 				detail: 'full',
 				apiKey: process.env.OMDBAPI_KEY,
-			})
-			.then((data) => {
-				const { body } = data;
+			});
 
-				if (body.Error) {
-					throw new Error(body.Error);
-				}
+		if (body.Error) {
+			throw new Error(body.Error);
+		}
 
-				if (body.Type !== 'movie') {
-					return responder.error('movie.notAMovie', msg.displayPrefix, args.join(' ')).send();
-				}
+		if (body.Type !== 'movie') {
+			return responder.error('movie.notAMovie', msg.displayPrefix, args.join(' ')).send();
+		}
 
-				const embed = {
-					title: body.Title,
-					description: body.Plot += body.Awards !== 'N/A' ? `\n\n${body.Awards}` : '',
-					url: body.Website !== 'N/A' ? body.Website : null,
-					fields: [
-						{
-							name: 'Ratings',
-							value: body.Ratings.map(r => `${r.Value} - ${r.Source}`).join('\n'),
-							inline: true,
-						}, {
-							name: 'Actors',
-							value: body.Actors.split(',').join('\n'),
-							inline: true,
-						}, {
-							name: 'Language',
-							value: body.Language,
-							inline: true,
-						}, {
-							name: 'Genre',
-							value: body.Genre,
-							inline: true,
-						}, {
-							name: 'Released',
-							value: (new Date(body.Released)).toLocaleDateString(),
-							inline: true,
-						}, {
-							name: 'Runtime',
-							value: body.Runtime,
-							inline: true,
-						}, {
-							name: 'Directors',
-							value: body.Director,
-							inline: true,
-						}, {
-							name: 'Rated',
-							value: body.Rated,
-							inline: true,
-						},
-					],
-					thumbnail: {
-						url: body.Poster,
-					},
-					timestamp: new Date(),
-				};
+		const embed = {
+			title: body.Title,
+			description: body.Plot += body.Awards !== 'N/A' ? `\n\n${body.Awards}` : '',
+			url: body.Website !== 'N/A' ? body.Website : null,
+			fields: [
+				{
+					name: 'Ratings',
+					value: body.Ratings.map(r => `${r.Value} - ${r.Source}`).join('\n'),
+					inline: true,
+				}, {
+					name: 'Actors',
+					value: body.Actors.split(',').join('\n'),
+					inline: true,
+				}, {
+					name: 'Language',
+					value: body.Language,
+					inline: true,
+				}, {
+					name: 'Genre',
+					value: body.Genre,
+					inline: true,
+				}, {
+					name: 'Released',
+					value: (new Date(body.Released)).toLocaleDateString(),
+					inline: true,
+				}, {
+					name: 'Runtime',
+					value: body.Runtime,
+					inline: true,
+				}, {
+					name: 'Directors',
+					value: body.Director,
+					inline: true,
+				}, {
+					name: 'Rated',
+					value: body.Rated,
+					inline: true,
+				},
+			],
+			thumbnail: {
+				url: body.Poster,
+			},
+			timestamp: new Date(),
+		};
 
-				return responder.embed(embed).send();
-			})
-			.catch(() => responder.error('general.restError').send());
+		return responder.embed(embed).send();
 	}
 };
 
