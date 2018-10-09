973c82e10d6c3632d40e40867da00f013aa53602
Nightlife <captainminenerd@gmail.com>
2018-10-09 08:06:43 +0800

    commands(utilities): "show" cleanup

--- src/commands/Utilities/show.js
+++ src/commands/Utilities/show.js
@@ -21,7 +21,7 @@ module.exports = class Show extends Command {
 				detail: 'full',
 				apiKey: process.env.OMDBAPI_KEY,
 			});
-
+			
 		if (body.Error) {
 			throw new Error(body.Error);
 		}
@@ -30,53 +30,50 @@ module.exports = class Show extends Command {
 			return responder.error('series.notASeries', msg.displayPrefix, args.join(' ')).send();
 		}
 
-		const embed = {
+		// todo: hey wouldn't it be fun if we made this have the ability to go through episodes
+		return responder.embed({
 			title: body.Title,
-			description: body.Plot += body.Awards !== 'N/A' ? `\n\n${body.Awards}` : '',
+			description: (body.plot + (body.Awards ? `\n\n${body.Awards}` : '')).substring(0, 2048),
 			url: body.Website !== 'N/A' ? body.Website : null,
-			fields: [
-				{
-					name: 'Ratings',
-					value: body.Ratings.map(r => `${r.Value} - ${r.Source}`).join('\n'),
-					inline: true,
-				}, {
-					name: 'Actors',
-					value: body.Actors.split(',').join('\n'),
-					inline: true,
-				}, {
-					name: 'Language',
-					value: body.Language,
-					inline: true,
-				}, {
-					name: 'Genre',
-					value: body.Genre,
-					inline: true,
-				}, {
-					name: 'Released',
-					value: (new Date(body.Released)).toLocaleDateString(),
-					inline: true,
-				}, {
-					name: 'Runtime',
-					value: body.Runtime,
-					inline: true,
-				}, {
-					name: 'Directors',
-					value: body.Director,
-					inline: true,
-				}, {
-					name: 'Rated',
-					value: body.Rated,
-					inline: true,
-				},
-			],
+			fields: [{
+				name: 'Language',
+				value: body.Language,
+				inline: true,
+			}, {
+				name: 'Genre',
+				value: body.Genre,
+				inline: true,
+			}, {
+				name: 'Released',
+				value: (new Date(body.Released)).toLocaleDateString(),
+				inline: true,
+			}, {
+				name: 'Runtime',
+				value: body.Runtime,
+				inline: true,
+			}, {
+				name: 'Directors',
+				value: body.Director,
+				inline: true,
+			}, {
+				name: 'Rated',
+				value: body.Rated,
+				inline: true,
+			},
+			{
+				name: 'Ratings',
+				value: body.Ratings.map(r => `${r.Value} - ${r.Source}`).join('\n'),
+				inline: true,
+			}, {
+				name: 'Actors',
+				value: body.Actors.split(',').join('\n'),
+				inline: true,
+			}],
 			thumbnail: {
 				url: body.Poster,
 			},
 			timestamp: new Date(),
-		};
-
-		// TODO: add reactions for every season :D
-		return responder.embed(embed).send();
+		}).send();
 	}
 };
 
