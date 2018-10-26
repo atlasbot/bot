4acd2f76c6743c74334ab1b54993ecf60769cd45
Nightlife <captainminenerd@gmail.com>
2018-10-26 16:35:03 +0800

    plugins(misc): "info" command cleanup

--- src/commands/Misc/info.js
+++ src/commands/Misc/info.js
@@ -11,40 +11,20 @@ module.exports = class Info extends Command {
 		const responder = new this.Atlas.structs.Responder(msg);
 
 		responder.embed({
-			author: {
-				icon_url: this.Atlas.client.avatarURL || this.Atlas.client.defaultAvatarURL,
-				name: 'commands.info.embed.title',
+			title: 'commands.info.title',
+			thumbnail: {
+				url: this.Atlas.client.user.avatarURL,
 			},
-			description: ['commands.info.embed.description', msg.displayPrefix],
+			description: ['commands.info.description', this.Atlas.commands.labels.size.toLocaleString()],
 			fields: [
 				{
-					name: 'commands.info.embed.author.name',
-					value: 'commands.info.embed.author.value',
+					name: 'commands.info.support.name',
+					value: 'commands.info.support.value',
 					inline: true,
 				},
 				{
-					name: 'commands.info.embed.invite.name',
-					value: 'commands.info.embed.invite.value',
-					inline: true,
-				},
-				{
-					name: 'commands.info.embed.support.name',
-					value: 'commands.info.embed.support.value',
-					inline: true,
-				},
-				{
-					name: 'commands.info.embed.documentation.name',
-					value: 'commands.info.embed.documentation.value',
-					inline: true,
-				},
-				{
-					name: 'commands.info.embed.status.name',
-					value: 'commands.info.embed.status.value',
-					inline: true,
-				},
-				{
-					name: 'commands.info.embed.website.name',
-					value: 'commands.info.embed.website.value',
+					name: 'commands.info.website.name',
+					value: 'commands.info.website.value',
 					inline: true,
 				},
 			],
