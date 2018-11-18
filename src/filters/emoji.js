40d97826b6fded4d1622bf2b1e4892081c449b6e
Nightlife <captainminenerd@gmail.com>
2018-11-18 09:35:19 +0800

    filters: excessive emoji filter

---
+++ src/filters/emoji.js
@@ -0,0 +1,28 @@
+const Filter = require('./../structures/Filter');
+
+const CUSTOM_EMOJI_REGEX = /<:([A-z0-9-_]+):([0-9]+)>/g;
+
+module.exports = class Mentions extends Filter {
+	constructor(Atlas) {
+		super(Atlas, module.exports.info);
+		this.Atlas = Atlas;
+	}
+
+	execute(str, msg, {
+		filterConfig,
+	}) {
+		const { threshold } = filterConfig;
+
+		const defaultEmojis = (str.match(this.Atlas.lib.utils.emoji.regex()) || '').length;
+		// checking to makes sure the emojis are valid may be worth while
+		const customEmojis = (str.match(CUSTOM_EMOJI_REGEX) || '').length;
+
+		return (defaultEmojis + customEmojis) > threshold;
+	}
+};
+
+module.exports.info = {
+	name: 'Emoji',
+	settingsKey: 'emoji',
+	description: 'Detects and removes messages with excessive emojis.',
+};
