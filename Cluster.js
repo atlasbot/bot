d082aab05b2f8a7d3fec93ef3639ea58e391ea06
Nightlife <captainminenerd@gmail.com>
2018-11-30 22:21:11 +0800

    undo last commit cus wrong branch lul
    This reverts commit cec6aad25769a017f975310a1db4c099b578fb95.

--- Cluster.js
+++ Cluster.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const { Manager } = require('./src/sharding');
 
-if (!process.env.TOKEN) {
-	require('dotenv').config();
-}
+require('dotenv').config();
 
 new Manager(process.env.TOKEN, path.join(__dirname, './Atlas.js'), { // eslint-disable-line no-new
 	guildsPerShard: 1500,
