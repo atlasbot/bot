085fc5528a86dcb8f896a2e4dba0c8c657a2ee6f
Nightlife <captainminenerd@gmail.com>
2018-12-12 06:38:03 +0800

    tags: "channel.lastMessageID" tag

---
+++ src/tagengine/tags/channel/lastMessageID.js
@@ -0,0 +1,21 @@
+const middleware = require('./middleware');
+const TagError = require('../../TagError');
+
+module.exports = middleware(({ channel }) => {
+	if (channel.type !== 0) {
+		throw new TagError('This tag only works for text channels.');
+	}
+
+	return channel.topic;
+});
+
+module.exports.info = {
+	name: 'channel.lastMessageID',
+	description: 'Gets the ID of the last message in the channel.',
+	args: '<channel>',
+	examples: [{
+		input: '{channel.lastMessageID}',
+		output: '520723523166011403',
+	}],
+	dependencies: ['channel'],
+};
