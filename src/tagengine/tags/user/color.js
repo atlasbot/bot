13344312e8761fee26723d6dd5ef8a75695f8942
Nightlife <captainminenerd@gmail.com>
2018-12-13 16:20:56 +0800

    tags: {user.color} tag

---
+++ src/tagengine/tags/user/color.js
@@ -0,0 +1,30 @@
+const middleware = require('./middleware');
+const roleColor = require('../../../../lib/utils/roleColor');
+
+module.exports = middleware(({ user, guild }, [hex = true]) => {
+	const member = guild.members.get(user.id);
+	const role = member.highestRole;
+
+	if (hex === 'true') {
+		return `#${roleColor(role.color)}`;
+	}
+
+	return role.color;
+}, 1);
+
+module.exports.info = {
+	name: 'user.color',
+	description: 'Gets the user\'s color, based on their highest role. If <hex=true>, the output will be in hex (#ffffff).',
+	args: '<hex=true> <user>',
+	examples: [{
+		input: '{user.color}',
+		output: '#95a5a6',
+	}, {
+		input: '{user.color;true}',
+		output: '9807270',
+	}, {
+		input: '{user.color;true;Atlas}',
+		output: '#03a9f4',
+	}],
+	dependencies: ['user', 'guild'],
+};
