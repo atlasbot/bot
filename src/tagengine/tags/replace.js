ddda35d8b75374ff4495acb4c036c0d588acdb32
Nightlife <captainminenerd@gmail.com>
2018-12-20 00:10:30 +0800

    tags: "replace" supports regex

--- src/tagengine/tags/replace.js
+++ src/tagengine/tags/replace.js
@@ -1,13 +1,24 @@
+const isSafeRe = require('safe-regex');
 const TagError = require('../TagError');
 const escapeRegex = require('../../../lib/utils/escapeRegex');
 
-module.exports = async (context, [string, phrase, replacement]) => {
-	if (!string || !phrase || !replacement) {
-		throw new TagError('"string", "phrase" and "replacement" are required.');
+module.exports = async (context, [string, search, replacement]) => {
+	if (!string || !search || !replacement) {
+		throw new TagError('"string", "search" and "replacement" are required.');
+	}
+
+	if (isSafeRe(search)) {
+		try {
+			const regex = new RegExp(search, 'ig');
+
+			if (regex) {
+				return string.replace(regex, replacement);
+			}
+		} catch (e) {} // eslint-disable-line no-empty
 	}
 
 	// safe case insensitive replace
-	const safe = escapeRegex(phrase);
+	const safe = escapeRegex(search);
 	const regex = new RegExp(safe, 'ig');
 
 	return string.replace(regex, replacement);
@@ -16,7 +27,7 @@ module.exports = async (context, [string, phrase, replacement]) => {
 module.exports.info = {
 	name: 'replace',
 	args: '<string> <search> <replacement>',
-	description: 'Returns a random argument.',
+	description: 'Returns a random argument, now with extra regexp flavour.',
 	examples: [{
 		input: '{replace;kittens are the best;kittens;puppies}',
 		output: 'puppies are the best',
@@ -24,6 +35,10 @@ module.exports.info = {
 		input: '{replace;Kittens are the best;kittens;puppies}',
 		output: 'puppies are the best',
 		note: 'Note the capitalized "Kittens". {replace} is case-insensitive.',
+	}, {
+		input: '{replace;The quick brown fox jumped over the lazy fox;([A-z]+);($1)}',
+		output: '(The) (quick) (brown) (fox) (jumped) (over) (the) (lazy) (fox)',
+		note: 'This example uses regular expressions, essentially surrounding groups of a-z in (). Google is your friend, but chances are you should just leave this alone if you don\'t understand interface.',
 	}],
 	dependencies: [],
 };
