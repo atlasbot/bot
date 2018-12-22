aca595f84fe3ce640367beb226444a86b00809d2
Nightlife <captainminenerd@gmail.com>
2018-12-23 03:17:39 +0800

    plugins(fun): "fact" command

---
+++ src/commands/Fun/fact.js
@@ -0,0 +1,20 @@
+const Command = require('../../structures/Command.js');
+const facts = require('../../../data/facts.json');
+
+module.exports = class Neat extends Command {
+	constructor(Atlas) {
+		super(Atlas, module.exports.info);
+	}
+
+	async action(msg) {
+		const responder = new this.Atlas.structs.Responder(msg);
+
+		const fact = this.Atlas.lib.utils.pickOne(facts);
+
+		return responder.localised().text(fact).send();
+	}
+};
+
+module.exports.info = {
+	name: 'fact',
+};
