b3db59967727de2a9231263b66312c50dfd0c7b2
Nightlife <captainminenerd@gmail.com>
2018-12-24 02:29:16 +0800

    plugins(utilities): "convert" command

---
+++ src/commands/Utilities/convert.js
@@ -0,0 +1,140 @@
+const superagent = require('superagent');
+const convert = require('convert-units');
+
+const Command = require('../../structures/Command.js');
+const Cache = require('../../../lib/structures/Cache');
+
+const cache = new Cache('currencies');
+
+// REGEX ALL THE THINGS! /s
+// regex was just the easiest solution i could think of
+const REGEX = /([0-9,. ]+) ?([A-z]{0,10}) (?:to )?([A-z]{0,4})/i;
+
+module.exports = class Convert extends Command {
+	constructor(Atlas) {
+		super(Atlas, module.exports.info);
+	}
+
+	async action(msg, args) {
+		const responder = new this.Atlas.structs.Responder(msg, msg.lang, 'convert');
+
+		if (!args.length) {
+			return responder.embed(this.helpEmbed(msg)).send();
+		}
+
+		let [, uncleanUnits, from, to] = REGEX.exec(args.join(' ')) || []; // eslint-disable-line prefer-const
+
+		const units = Number((uncleanUnits || '').replace(/[^0-9]/g, ''));
+
+		if (!units) {
+			return responder.error('invalidUnits').send();
+		}
+
+		if (!from) {
+			return responder.error('invalidFrom').send();
+		}
+
+		if (!to) {
+			return responder.error('invalidTo').send();
+		}
+
+		let currency = {
+			base: 'EUR',
+			rates: {},
+		};
+		// try/catch getCurrencies() so it doesn't nuke the whole command if currency conversion is offline
+		try {
+			currency = await this.getCurrencies();
+		} catch (e) {} // eslint-disable-line no-empty
+
+		if (from.toUpperCase() in currency.rates) {
+			if (!(to.toUpperCase() in currency.rates)) {
+				return responder.error('currency.invalidTarget', to.toLowerCase()).send();
+			}
+
+			const converted = units * this.getRate(from.toUpperCase(), to.toUpperCase(), currency);
+
+			const fromFormed = new Intl.NumberFormat('en-AU', {
+				style: 'currency',
+				currency: from,
+			}).format(units);
+
+			const toFormed = new Intl.NumberFormat('en-AU', {
+				style: 'currency',
+				currency: to,
+			}).format(converted);
+
+			return responder
+				.text('currency.converted', fromFormed, from.toUpperCase(), toFormed, to.toUpperCase())
+				.send();
+		}
+
+		const possible = convert().possibilities();
+
+		if (!possible.includes(from)) {
+			from = possible.find(t => t.toLowerCase() === from.toLowerCase());
+
+			if (!from) {
+				return responder.error('other.unknownInput').send();
+			}
+		}
+
+		if (!possible.includes(to)) {
+			to = possible.find(t => t.toLowerCase() === to.toLowerCase());
+
+			if (!to) {
+				return responder.error('other.unknownOutput').send();
+			}
+		}
+
+		const converted = convert(units).from(from).to(to).toFixed(2);
+
+		return responder.text('other.converted', units, from, converted, to).send();
+	}
+
+	getRate(from, to, { base, rates }) {
+		if (!rates[to] || !rates[from]) {
+			throw new Error('Invalid currency');
+		}
+
+		if (from === base) {
+			return rates[to];
+		}
+
+		if (to === base) {
+			return 1 / rates[from];
+		}
+
+		return rates[to] * (1 / rates[from]);
+	}
+
+	async getCurrencies() {
+		const cached = await cache.get('currencies');
+
+		if (cached) {
+			return cached;
+		}
+
+		const { body } = await superagent.get('https://api.exchangeratesapi.io/latest');
+
+		body.rates[body.base] = 1;
+
+		// cache for 5 minutes
+		await cache.set('currencies', body, 300);
+
+		return body;
+	}
+};
+
+module.exports.info = {
+	name: 'convert',
+	examples: [
+		'1lb kg',
+		'10gb mb',
+		'1 usd aud',
+		'12,000gb to mb',
+		'1000mm cm',
+		'10eur usd',
+		'1.75l ml',
+	],
+};
