431ee92ff4748249e3856b2216245a92c4e0969e
Nightlife <captainminenerd@gmail.com>
2018-12-29 22:41:23 +0800

    plugins(roles): reaction roles

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -7,6 +7,12 @@ module.exports = class Event {
 		const user = this.Atlas.client.users.get(userID);
 		const collectors = this.Atlas.collectors.emojis.get(msg.id) || this.Atlas.collectors.emojis.get(userID);
 
+		// strip emoji skin because it fucks with things
+		// emoji.id is only set for custom emojis, otherwise atlas uses the emoji as the content
+		const emid = this.Atlas.lib.utils.cleanEmoji(emoji.id || emoji.name);
+
+		console.warn(emid);
+
 		if (userID === this.Atlas.client.user.id) {
 			return;
 		}
@@ -19,32 +25,59 @@ module.exports = class Event {
 			collectors.forEach(c => c.fire(msg, emoji, userID));
 		}
 
-		// in a dev environment this will let people re-run commands
-		if (this.Atlas.env === 'development' && emoji.name === '🔁') {
-			if (userID === msg.author.id) {
-				try {
-					await this.Atlas.client.purgeChannel(msg.channel.id, 100, m => m.author.bot, null, msg.id);
-
-					await this.Atlas.client.removeMessageReaction(
-						msg.channel.id,
-						msg.id,
-						'🔁',
-						userID,
-					);
-				} catch (e) {} // eslint-disable-line no-empty
-
-				this.Atlas.client.emit('messageCreate', msg);
-			}
-		}
-
 		if (msg.guild && user) {
 			const settings = await this.Atlas.DB.getSettings(msg.guild.id);
+
+			const roles = settings.plugin('roles');
+
+			if (roles.state === 'enabled' && roles.options.reactions.length) {
+				const reactions = roles.options.reactions.filter((r) => {
+					if (r.channel && r.channel !== msg.channel.id) {
+						return false;
+					}
+
+					if (r.message && r.message !== msg.id) {
+						return false;
+					}
+
+					return r.emoji === emid;
+				});
+
+				const member = msg.guild.members.get(userID);
+
+				const toAdd = reactions
+					.map(r => msg.guild.roles.get(r.role))
+					.filter(r => r && !r.higherThan(msg.guild.me.highestRole) && !member.roles.includes(r.id));
+
+				if (toAdd.length) {
+					const added = [];
+					for (const role of toAdd.slice(0, 3)) {
+						try {
+							await member.addRole(role.id);
+
+							added.push(role);
+						} catch (e) {
+							console.warn(e);
+						}
+					}
+
+					if (added.length) {
+						const responder = new this.Atlas.structs.Responder(msg, settings.lang);
+
+						const raw = emoji.id ? `<:${emoji.name}:${emoji.id}>` : emoji.name;
+
+						responder
+							.dm(userID)
+							.text('general.roles.reactionAdd', added.map(r => r.name).join('`, `'), raw)
+							.send().catch(console.warn);
+					}
+				}
+			}
 
 			await settings.runActions({
 				guild: msg.guild.id,
 				'trigger.type': 'reactionAdd',
-				// emoji.id is only set for custom emojis, otherwise atlas uses the emoji as the content
-				'trigger.content': emoji.id || emoji.name,
+				'trigger.content': emid,
 			}, {
 				msg,
 				user,
--- src/events/messageReactionRemove.js
+++ src/events/messageReactionRemove.js
@@ -6,6 +6,8 @@ module.exports = class Event {
 	async execute(msg, emoji, userID) {
 		const user = this.Atlas.client.users.get(userID);
 
+		const emid = this.Atlas.lib.utils.cleanEmoji(emoji.id || emoji.name);
+
 		if (userID === this.Atlas.client.user.id) {
 			return;
 		}
@@ -17,11 +19,59 @@ module.exports = class Event {
 		if (msg.guild && user) {
 			const settings = await this.Atlas.DB.getSettings(msg.guild.id);
 
+			const roles = settings.plugin('roles');
+
+			if (roles.state === 'enabled' && roles.options.reactions.length) {
+				const reactions = roles.options.reactions.filter((r) => {
+					if (r.channel && r.channel !== msg.channel.id) {
+						return false;
+					}
+
+					if (r.message && r.message !== msg.id) {
+						return false;
+					}
+
+					return r.emoji === emid;
+				});
+
+				const member = msg.guild.members.get(userID);
+
+				const toRemove = reactions
+					.map(r => msg.guild.roles.get(r.role))
+					.filter(r => r && !r.higherThan(msg.guild.me.highestRole) && member.roles.includes(r.id));
+
+				if (toRemove.length) {
+					// max 3 roles per reaction
+
+					const removed = [];
+					for (const role of toRemove.slice(0, 3)) {
+						try {
+							await member.removeRole(role.id);
+
+							removed.push(role);
+						} catch (e) {
+							console.warn(e);
+						}
+					}
+
+					if (removed.length) {
+						const responder = new this.Atlas.structs.Responder(msg, settings.lang);
+
+						const raw = emoji.id ? `<:${emoji.name}:${emoji.id}>` : emoji.name;
+
+						responder
+							.dm(userID)
+							.text('general.roles.reactionRemove', removed.map(r => r.name).join('`, `'), raw)
+							.send().catch(console.warn);
+					}
+				}
+			}
+
 			await settings.runActions({
 				guild: msg.guild.id,
 				'trigger.type': 'reactionRemove',
 				// emoji.id is only set for custom emojis, otherwise atlas uses the emoji as the content
-				'trigger.content': emoji.id || emoji.name,
+				'trigger.content': emid,
 			}, {
 				msg,
 				user,
