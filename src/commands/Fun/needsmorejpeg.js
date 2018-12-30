d64885ecfd55663dcb043c50b6047e3e54c3d6f2
Nightlife <captainminenerd@gmail.com>
2018-12-30 22:11:00 +0800

    plugins(fun): "needsmorejpeg" searches last messages for url/attachment

--- src/commands/Fun/needsmorejpeg.js
+++ src/commands/Fun/needsmorejpeg.js
@@ -9,7 +9,18 @@ module.exports = class NeedsMoreJPEG extends Command {
 	async action(msg, args) {
 		const responder = new this.Atlas.structs.Responder(msg, msg.lang, 'needsmorejpeg');
 
-		const url = (msg.attachments[0] && msg.attachments[0].url) || args[0];
+		let url = (msg.attachments[0] && msg.attachments[0].url) || args[0];
+		if (!url || !this.Atlas.lib.utils.isUri(url)) {
+			// search messages over the last 15s for attachments/links
+			// not 100% sure about this yet but /shrug
+			for (const m of msg.channel.messages.filter(x => (Date.now() - x.timestamp) < 15000)) {
+				url = (m.attachments[0] && m.attachments[0].url) || (this.Atlas.lib.utils.isUri(m.content) && m.content);
+
+				if (url) {
+					break;
+				}
+			}
+		}
 
 		if (!url) {
 			return responder.error('noArgs').send();
@@ -23,7 +34,7 @@ module.exports = class NeedsMoreJPEG extends Command {
 
 		let quality = this.Atlas.lib.utils.parseNumber(args[1]);
 		if (isNaN(quality)) {
-			quality = 1.5;
+			quality = 2.5;
 		}
 
 		const buffer = await image.quality(quality).getBufferAsync(jimp.MIME_JPEG);
