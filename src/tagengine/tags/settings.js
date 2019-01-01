fe1f5d0ff2327dc8764702ea6f9b7a40bb3bc35c
Nightlife <captainminenerd@gmail.com>
2019-01-01 09:54:48 +0800

    tags: {settings} tag

---
+++ src/tagengine/tags/settings.js
@@ -0,0 +1,25 @@
+const TagError = require('../TagError');
+
+module.exports = async ({ settings, Atlas }, [key]) => {
+	if (!key) {
+		throw new TagError('"key" is required.');
+	}
+
+	const data = Atlas.lib.utils.getNested(settings.raw, key);
+
+	return JSON.stringify(data).replace(/^"(.*)"$/, '$1');
+};
+
+module.exports.info = {
+	name: 'settings',
+	args: '<key>',
+	description: 'Gets a raw setting value. This is an advanced command, if you don\'t understand how to use it or what it does, then you probably shouldn\'t be using it.',
+	examples: [{
+		input: '{settings;prefix}',
+		output: 'a!',
+	}, {
+		input: '{settings;plugins.moderation.state}',
+		output: 'enabled',
+	}],
+	dependencies: ['settings'],
+};
