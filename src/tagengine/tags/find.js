6f20f163ed5315d3eff422c9980b8ae3f9da1bdb
Nightlife <captainminenerd@gmail.com>
2019-01-04 15:58:07 +0800

    tags: some minor overhauls to {find} with groups and regex flags

--- src/tagengine/tags/find.js
+++ src/tagengine/tags/find.js
@@ -2,21 +2,32 @@ const isSafeRe = require('safe-regex');
 const escapeRegex = require('atlas-lib/lib/utils/escapeRegex');
 const TagError = require('../TagError');
 
-module.exports = async (context, [string, search = '']) => {
+module.exports = async ({ Atlas }, [string, search = '', group = '1', flags = 'i']) => {
 	if (!string) {
 		throw new TagError('"target" is required.');
 	}
 
+	group = Atlas.lib.utils.parseNumber(group);
+	if (isNaN(group)) {
+		throw new TagError('"group" must be a number');
+	}
+
 	if (isSafeRe(search)) {
 		try {
-			const regex = new RegExp(search, 'ig');
+			const regex = new RegExp(search, flags);
 
 			if (regex) {
 				const match = string.match(regex);
 
 				if (match) {
+					if (match[group] !== undefined) {
+						return match[group];
+					}
+
 					return match.join(' ');
 				}
+
+				return;
 			}
 		} catch (e) {} // eslint-disable-line no-empty
 	}
@@ -34,8 +45,8 @@ module.exports = async (context, [string, search = '']) => {
 
 module.exports.info = {
 	name: 'find',
-	args: '<target> <search>',
-	description: 'Finds <search> in <target>, now with extra regexp flavour.',
+	args: '<target> <search> <group> <flags=i>',
+	description: 'Finds <search> in <target>, now with extra regexp flavour. <group> is an optional capture group to return when using regex. <flags> are any regex flags like "g" or "i", you can mix and match groups like "gi".',
 	examples: [{
 		input: '{find;This is a test;test}',
 		output: 'test',
@@ -43,9 +54,9 @@ module.exports.info = {
 		input: '{find;This does not include the forbidden word;test}',
 		output: '',
 	}, {
-		input: '{find;This does not include the forbidden word;[d]+}',
-		output: 'd d dd d',
-		note: 'This will return all "d" characters in the string using regex.',
+		input: '{find;The date is 07-08-2018;([0-9]{1,2})(?:/|-)([0-9]{1,2})(?:/|-)([0-9]{2,4});3}',
+		output: '2018',
+		note: 'Using some regex magic, this will match dates and return the year using the third capture group. This is an advanced example.',
 	}],
 	dependencies: [],
 };
