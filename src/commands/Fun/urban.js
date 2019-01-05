96b32e1b0db64a49d045e79e4fc999cd7186102c
Nightlife <captainminenerd@gmail.com>
2019-01-05 12:25:24 +0800

    plugins(fun): "urban" 404 fix

--- src/commands/Fun/urban.js
+++ src/commands/Fun/urban.js
@@ -20,41 +20,49 @@ module.exports = class extends Command {
 			return responder.error('nsfwWord').send();
 		}
 
-		const { body } = await superagent.get('http://api.urbandictionary.com/v0/define')
-			.query({
-				term: args.join(' '),
-			})
-			.set('User-Agent', this.Atlas.userAgent);
+		try {
+			const { body } = await superagent.get('http://api.urbandictionary.com/v0/define')
+				.query({
+					term: args.join(' '),
+				})
+				.set('User-Agent', this.Atlas.userAgent);
 
-		if (body.result_type === 'no_results') {
-			return responder.error('noResults', args.join(' '));
-		}
-
-		const { word, author, thumbs_up, thumbs_down, definition } = body.list.reduce((a, b) => {
-			if ((a.thumbs_up - a.thumbs_down) > (b.thumbs_up - b.thumbs_down)) {
-				return a;
+			if (body.result_type === 'no_results' || !body.list.length) {
+				return responder.error('noResults', args.join(' ')).send();
 			}
 
-			return b;
-		});
+			const { word, author, thumbs_up, thumbs_down, definition } = body.list.reduce((a, b) => {
+				if ((a.thumbs_up - a.thumbs_down) > (b.thumbs_up - b.thumbs_down)) {
+					return a;
+				}
 
-		if (!msg.channel.nsfw && swearjar.profane(definition)) {
-			return responder.error('nsfwDef').send();
+				return b;
+			});
+
+			if (!msg.channel.nsfw && swearjar.profane(definition)) {
+				return responder.error('nsfwDef').send();
+			}
+
+			return responder.embed({
+				title: word,
+				description: definition.replace(/\[|\]/g, '').substring(0, 2048),
+				url: `https://www.urbandictionary.com/define.php?term=${encodeURIComponent(word)}`,
+				fields: [{
+					name: 'author',
+					value: `[${author}](https://www.urbandictionary.com/author.php?author=${encodeURIComponent(author)})`,
+				}],
+				footer: {
+					text: ['votes', thumbs_up, thumbs_down],
+				},
+				timestamp: new Date(),
+			}).send();
+		} catch (e) {
+			if (e.status === 404) {
+				return responder.error('noResults', args.join(' '));
+			}
+
+			throw e;
 		}
-
-		return responder.embed({
-			title: word,
-			description: definition.replace(/\[|\]/g, '').substring(0, 2048),
-			url: `https://www.urbandictionary.com/define.php?term=${encodeURIComponent(word)}`,
-			fields: [{
-				name: 'author',
-				value: `[${author}](https://www.urbandictionary.com/author.php?author=${encodeURIComponent(author)})`,
-			}],
-			footer: {
-				text: ['votes', thumbs_up, thumbs_down],
-			},
-			timestamp: new Date(),
-		}).send();
 	}
 };
 
