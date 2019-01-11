1554270624a1bf7ea434b9b27168b341dabc989f
Nightlife <captainminenerd@gmail.com>
2019-01-12 07:02:45 +0800

    fix: itme

--- src/autoscale.js
+++ src/autoscale.js
@@ -1,4 +1,4 @@
-// at the itme of writing this i just want to play with kubernete :D
+// for the most part i'm playing with kubernetes for fun, i understand it's overkill
 // from https://github.com/TheSharks/WildBeast/blob/df03e9402d7690cacc9735def80eff2de5263b77/src/internal/k8s-autoscale.js
 const os = require('os');
 // const superagent = require('superagent');
