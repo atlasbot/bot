7f56dbcfb3520ad62fbf531c54d6a10a3978db90
Nightlife <captainminenerd@gmail.com>
2019-01-20 05:55:35 +0800

    fix(schemas): update embed limits

--- src/schemas/embed.js
+++ src/schemas/embed.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 module.exports = {
 	color: Joi.number(),
 	title: Joi.string().max(256).allow(null, ''),
-	description: Joi.string().max(2048).allow([true, false, null, '']),
+	description: Joi.string().max(2000).allow([true, false, null, '']),
 	fields: Joi.array().items({
 		name: Joi.string().max(256),
 		value: Joi.alternatives([
