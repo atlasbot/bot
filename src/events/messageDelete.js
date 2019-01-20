df31103d316ca95922046e68e4fb0176697749f4
Nightlife <captainminenerd@gmail.com>
2019-01-21 05:26:29 +0800

    refactor(events): cleanup messageDelete

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -6,75 +6,77 @@ module.exports = class {
 	async execute(msg) {
 		this.Atlas.sent = this.Atlas.sent.filter(s => s.msg.id !== msg.id);
 
-		if (msg.type === 0 && msg.guild && !msg.author.bot) {
-			if (this.Atlas.deleteAliases.has(msg.id)) {
-				const x = this.Atlas.deleteAliases.get(msg.id);
+		if (msg.type !== 0 || !msg.guild || msg.author.bot || !msg.content) {
+			return;
+		}
 
-				this.Atlas.client.deleteMessage(x.channel, x.msg).catch(console.error);
-			}
+		if (this.Atlas.deleteAliases.has(msg.id)) {
+			const x = this.Atlas.deleteAliases.get(msg.id);
 
-			const settings = await this.Atlas.DB.settings(msg.guild);
+			this.Atlas.client.deleteMessage(x.channel, x.msg).catch(console.error);
+		}
 
-			if (!settings.actionLogChannel) {
-				return;
-			}
+		const settings = await this.Atlas.DB.settings(msg.guild);
 
-			const auditEntry = await this.Atlas.util.getGuildAuditEntry(msg.guild, msg.id, 72);
+		if (!settings.actionLogChannel) {
+			return;
+		}
 
-			const embed = {
-				title: 'general.logs.messageDelete.title',
-				color: this.Atlas.colors.get('blue').decimal,
-				description: ['general.logs.messageDelete.description', msg.author.tag, msg.channel.mention],
-				fields: [{
-					name: 'general.logs.messageDelete.content.name',
-					value: msg.content.substring(0, 1024),
-				}],
-				thumbnail: {
-					url: msg.author.avatarURL,
-				},
-				footer: {
-					text: `User ${msg.author.id}`,
-				},
-				timestamp: new Date(),
-			};
+		const auditEntry = await this.Atlas.util.getGuildAuditEntry(msg.guild, msg.id, 72);
 
-			if (auditEntry) {
+		const embed = {
+			title: 'general.logs.messageDelete.title',
+			color: this.Atlas.colors.get('blue').decimal,
+			description: ['general.logs.messageDelete.description', msg.author.tag, msg.channel.mention],
+			fields: [{
+				name: 'general.logs.messageDelete.content.name',
+				value: msg.content.substring(0, 1024),
+			}],
+			thumbnail: {
+				url: msg.author.avatarURL,
+			},
+			footer: {
+				text: `User ${msg.author.id}`,
+			},
+			timestamp: new Date(),
+		};
+
+		if (auditEntry) {
+			embed.fields.push({
+				name: 'general.logs.messageDelete.moderator.name',
+				value: auditEntry.user.tag,
+				inline: true,
+			});
+
+			if (auditEntry.reason) {
 				embed.fields.push({
-					name: 'general.logs.messageDelete.moderator.name',
-					value: auditEntry.user.tag,
+					name: 'general.logs.messageDelete.reason.name',
+					value: auditEntry.reason,
 					inline: true,
 				});
-
-				if (auditEntry.reason) {
-					embed.fields.push({
-						name: 'general.logs.messageDelete.reason.name',
-						value: auditEntry.reason,
-						inline: true,
-					});
-				}
-
-				embed.footer.text += ` Mod ${auditEntry.user.id}`;
-
-				const types = [];
-				if (!msg.channel.topic || !msg.channel.topic.includes('actionlog-ignore')) {
-					types.push('action');
-				}
-
-				if (!msg.channel.topic || !msg.channel.topic.includes('modlog-ignore')) {
-					types.push('mod');
-				}
-
-				// if it was deleted by a moderator it's probably worth while to log it
-				await settings.log('mod', embed);
-
-				return settings.log('action', embed);
 			}
 
-			if (msg.channel.topic && msg.channel.topic.includes('actionlog-ignore')) {
-				return;
+			embed.footer.text += ` Mod ${auditEntry.user.id}`;
+
+			const types = [];
+			if (!msg.channel.topic || !msg.channel.topic.includes('actionlog-ignore')) {
+				types.push('action');
 			}
+
+			if (!msg.channel.topic || !msg.channel.topic.includes('modlog-ignore')) {
+				types.push('mod');
+			}
+
+			// if it was deleted by a moderator it's probably worth while to log it
+			await settings.log('mod', embed);
 
 			return settings.log('action', embed);
 		}
+
+		if (msg.channel.topic && msg.channel.topic.includes('actionlog-ignore')) {
+			return;
+		}
+
+		return settings.log('action', embed);
 	}
 };
