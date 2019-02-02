9da9f26bd5cc8b1e05fc3041ceea075c3b127740
Nightlife <captainminenerd@gmail.com>
2019-02-02 10:49:05 +0800

    fix(tags): "channel.lastMessageID" now works properly

--- src/tagengine/tags/channel/lastMessageID.js
+++ src/tagengine/tags/channel/lastMessageID.js
@@ -6,7 +6,7 @@ module.exports = middleware(({ channel }) => {
 		throw new TagError('This tag only works for text channels.');
 	}
 
-	return channel.topic;
+	return channel.lastMessageID;
 });
 
 module.exports.info = {
