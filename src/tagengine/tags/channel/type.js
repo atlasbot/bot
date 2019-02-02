24f18e398c4fa0adf7775af6d57d74bb07584646
Nightlife <captainminenerd@gmail.com>
2019-02-02 10:37:56 +0800

    feat(tags): merge "channel.cleanType" and "channel.type"

--- src/tagengine/tags/channel/type.js
+++ src/tagengine/tags/channel/type.js
@@ -1,22 +1,18 @@
 const middleware = require('./middleware');
-const TagError = require('../../TagError');
 
-module.exports = middleware(({ channel }) => {
-	if (channel.type !== 0) {
-		throw new TagError('This tag only works for text channels.');
-	}
-
-	return channel.type;
-});
+module.exports = middleware(({ channel, Atlas }) => Atlas.lib.utils.getChannelType(channel.type));
 
 module.exports.info = {
 	name: 'channel.type',
+	aliases: ['channel.cleanType'],
 	description: 'Gets the type of the channel.',
 	args: '<channel>',
 	examples: [{
 		input: '{channel.type}',
-		output: '0',
-		note: 'Full list of types available here: https://discordapp.com/developers/docs/resources/channel#channel-object-channel-types',
+		output: 'text',
+	}, {
+		input: '{channel.type;vc-1}',
+		output: 'voice',
 	}],
 	dependencies: ['channel'],
 };
